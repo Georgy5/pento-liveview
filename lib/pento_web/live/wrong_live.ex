9d71f574f76d385d86b6c9e56b668d7a4184e778
Karl Keller <kgk86@hotmail.com>
2025-04-24 11:52:43 +0200

    Add handle_event/3 to handle inbound events

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -26,4 +26,17 @@ defmodule PentoWeb.WrongLive do
   </h2>
   """
   end
+
+  def handle_event("guess", %{"number" => guess}, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again.mix"
+    score = socket.assigns.score - 1
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score
+      )
+    }
+  end
 end
