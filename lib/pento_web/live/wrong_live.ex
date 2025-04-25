8fb105d29977dc9c850464138ce67ca1742c06ef
Karl Keller <kgk86@hotmail.com>
2025-04-25 14:10:57 +0200

    Undo time() example
    The time() display was an example to show how LiveView knows what
    diffs to send and when.

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,7 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+    {:ok, assign(socket, score: 0, message: "Make a guess:")}
   end
 
   def render(assigns) do
@@ -11,7 +11,6 @@ defmodule PentoWeb.WrongLive do
     <h2>
       {@message}
     </h2>
-    <p>It's <%= @time %></p>
     <br />
     <h2>
       <%= for n <- 1..10 do %>
@@ -28,10 +27,6 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
-  def time() do
-    DateTime.utc_now |> to_string
-  end
-
   def handle_event("guess", %{"number" => guess}, socket) do
     message = "Your guess: #{guess}. Wrong. Guess again."
     score = socket.assigns.score - 1
@@ -40,8 +35,7 @@ defmodule PentoWeb.WrongLive do
       assign(
         socket,
         message: message,
-        score: score,
-        time: time()
+        score: score
       )
     }
   end
