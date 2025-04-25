02d370d73b655d8789c3b31a3accdbf9952522ac
Karl Keller <kgk86@hotmail.com>
2025-04-25 13:59:11 +0200

    Add a time display that re-renders on event change
    Include the time in the HEEx template.
    Use socket assigns value @time to track changes to the time,
    thus the @time value will be re-rendered.

--- lib/pento_web/live/wrong_live.ex
+++ lib/pento_web/live/wrong_live.ex
@@ -2,7 +2,7 @@ defmodule PentoWeb.WrongLive do
   use PentoWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, score: 0, message: "Make a guess:")}
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
   end
 
   def render(assigns) do
@@ -11,6 +11,7 @@ defmodule PentoWeb.WrongLive do
     <h2>
       {@message}
     </h2>
+    <p>It's <%= @time %></p>
     <br />
     <h2>
       <%= for n <- 1..10 do %>
@@ -27,6 +28,10 @@ defmodule PentoWeb.WrongLive do
     """
   end
 
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+
   def handle_event("guess", %{"number" => guess}, socket) do
     message = "Your guess: #{guess}. Wrong. Guess again."
     score = socket.assigns.score - 1
@@ -35,7 +40,8 @@ defmodule PentoWeb.WrongLive do
       assign(
         socket,
         message: message,
-        score: score
+        score: score,
+        time: time()
       )
     }
   end
